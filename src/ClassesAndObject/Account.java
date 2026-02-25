77f3b637300e182c07fef83705ce4c7d547e7f1f
Abhishesh <yadavabhi30404@gmail.com>
2026-02-26 00:40:02 +0530

    Add first program

---
+++ src/ClassesAndObject/Account.java
@@ -0,0 +1,60 @@
+package ClassesAndObject;
+
+import java.util.Date;
+
+public class Account {
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private Date dateCreated;
+    public Account(int id, double balance, double annualInterestRate){
+        this.id=id;
+        this.balance=balance;
+        this.annualInterestRate=annualInterestRate;
+        dateCreated=new Date();
+    }
+    public boolean withdraw(double amount){
+       /*if(balance>=amount){
+            balance-=amount;
+            return true;
+        }
+       else
+         return false;*/
+        //or we can write as
+        if(balance<amount) return false;
+
+        balance-=amount;
+        return true;
+    }
+    public void deposit(double amount){
+        balance+=amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+}
