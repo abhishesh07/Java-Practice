f927cb58f97b1e3d41e64ea4c02a46f3d8150341
Abhishesh <yadavabhi30404@gmail.com>
2026-02-26 19:28:46 +0530

    this is rectangleclass and its main method

---
+++ src/ClassesAndObject/Client.java
@@ -0,0 +1,59 @@
+package ClassesAndObject;
+
+import java.util.ArrayList;
+
+public class Client {
+    private int id;
+    private String name;
+    private String phone;
+    private ArrayList<Account> accounts; //null
+    public Client(int id, String name, String phone){
+        this.id=id;
+        this.name=name;
+        this.phone=phone;
+        accounts = new ArrayList<>();
+    }
+    public boolean addAccount(Account account){
+        accounts.add(account);
+        return true;
+    }
+    public boolean removeAccount(int accountId){
+        for(Account account :accounts)
+            if(account.getId() == accountId){
+                accounts.remove(account);
+                return true;
+            }
+        return false;
+    }
+    public String toString(){
+        String s= this.id+" "+ this.name +" "+ this.phone + " \n";
+        for(Account account:accounts)//if you remove loop here then you get message like [ ... ] and no error.
+            s+=account.toString() + "\n"; //here you can remove .toString method.
+
+        return s;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
